be43bfe0e23bcc3cfdef36f9939c4127b7468582
Wayne <sansugui@gmail.com>
2018-05-05 11:17:25 -0700

    sandbox is incomplete sandbox 2 merges a gbr w/ rfr

--- sandbox2.py
+++ sandbox2.py
@@ -1,36 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-
-
-class MLModel:
-
-    def __init__(self):
-    train = pd.DataFrame
-    test = pd.DataFrame
-    features = []
-    target_variable = []
-
-    def load_train_data(self, filename):
-        train = pd.read_csv(filename)
-
-    def load_test_data(self, filename):
-        test = pd.read_csv(filename)
-
-    def get_features(self):
-        features = self.train.columns[1:(len(self.train.columns) - 1)]
-
-    def get_target_variable(self):
-        target_variable = self.train.columns[len(self.train.columns) - 1]
-        target_variable = self.train.columns[len(self.train.columns) - 1]
-
-housing_prices = MLModel()
-
-housing_prices.load_train_data("train.csv")
-housing_prices.load_test_data("test.csv")
-
-housing_prices.get_features()
-housing_prices.get_target_variable()
+from sklearn.ensemble import GradientBoostingRegressor
 
 # load data
 df = pd.read_csv("train.csv")
@@ -61,9 +32,17 @@ test = test[train.columns]
 rfr = RandomForestRegressor(n_estimators=100)
 rfr.fit(train, raw_train[target_variable])
 
+gbc = GradientBoostingRegressor(n_estimators=100)
+gbc.fit(train, raw_train[target_variable])
+
 # predict model
-predictions = pd.DataFrame(rfr.predict(test))
-export = pd.concat([raw_test["Id"], predictions], axis = 1)
+rfr_predictions = pd.DataFrame(rfr.predict(test))
+gbc_predictions = pd.DataFrame(gbc.predict(test))
+
+predictions = pd.concat([rfr_predictions, gbc_predictions], axis = 1)
+predictions['avg'] = predictions.mean(axis = 1)
+
+export = pd.concat([raw_test["Id"], predictions['avg']], axis = 1)
 export_headers = ["Id", "SalePrice"]
 export.columns = export_headers
 
